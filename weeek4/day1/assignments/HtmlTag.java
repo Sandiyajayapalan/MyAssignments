335c03fd501d178e88f236f727916a9e182de82e
Sandiyajayapalan <112773493+Sandiyajayapalan@users.noreply.github.com>
2022-09-25 13:10:29 +0530

    Add files via upload
    weeek4day1assignments

---
+++ weeek4/day1/assignments/HtmlTag.java
@@ -0,0 +1,28 @@
+package weeek4.day1.assignments;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class HtmlTag {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver=new ChromeDriver();
+		driver.get(" https://html.com/tags/table/ ");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		List<WebElement> row = driver.findElements(By.xpath("//div[@class='render']/table//tr"));
+		System.out.println("The number of rows : " + row.size());
+		List<WebElement> columns = driver.findElements(By.xpath("//div[@class='render']/table//th"));
+		System.out.println("The number of columns : " + columns.size());
+	}
+
+}
